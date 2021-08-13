9d04becbb0d3b90853120a451c20834f7d5ec5e1
wangkun <wangkun@woodemi.com>
2021-08-13 08:16:55 +0800

    Add macOS project for plugin.brick
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ plugin/__brick__/{{project}}/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import {{project}}
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  {{project}}Plugin.register(with: registry.registrar(forPlugin: "{{project}}Plugin"))
+}
