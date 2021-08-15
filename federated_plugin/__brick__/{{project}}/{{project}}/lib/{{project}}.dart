aa731b8906a8b6f6d236a67e94b64716c1cf89b6
wangkun <wangkun@woodemi.com>
2021-08-15 19:11:51 +0800

    Remove federated_plugin
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- federated_plugin/__brick__/{{project}}/{{project}}/lib/{{project}}.dart
+++
@@ -1,8 +0,0 @@
-
-import 'dart:async';
-
-import 'package:{{project}}_platform_interface/{{project}}_platform_interface.dart';
-
-class {{project}} {
-  static Future<String?> get platformVersion => {{project}}Platform.instance.platformVersion;
-}
